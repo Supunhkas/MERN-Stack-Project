61890ae7d3372375623dce6840622ac72c13febb
supun_h <supunh.kas@gmail.com>
2022-12-23 08:47:34 +0530

    setup mongodv connection

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dontenv = require("dotenv");
 const app = express();
+require("dotenv").config();
 
 // create port to use locahost or server
 const PORT = process.env.PORT || 8070;
@@ -12,3 +13,23 @@ const PORT = process.env.PORT || 8070;
 app.use(cors());
 // set text to json format
 app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(URL, {
+  //   useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //   useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MOngo db Connection Succeess");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`);
+});
+
